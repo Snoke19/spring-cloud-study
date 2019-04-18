2c2b8bc29f47d32db19348b9fb0ad2fd5fe0b223
Java <oleksandr.mamchiI@gmail.com>
2019-04-18 18:18:06 +0300

    added new service

--- servercfn/src/main/java/com/config/servercfn/ServercfnApplication.java
+++ servercfn/src/main/java/com/config/servercfn/ServercfnApplication.java
@@ -1,14 +1,15 @@
-package com.cloud.licensingservice;
+package com.config.servercfn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-public class LicensingServiceApplication {
+@EnableConfigServer
+public class ServercfnApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(LicensingServiceApplication.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(ServercfnApplication.class, args);
+	}
 
 }
